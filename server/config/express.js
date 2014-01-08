be78572a1e4885f4c87877d5bdf8e65d170141fe
cgcladera <cgcladera@gmail.com>
2014-01-08 16:16:42 +0100

    Enables strict routing and redirects /theater to /theater/

--- server/config/express.js
+++ server/config/express.js
@@ -2,9 +2,9 @@ var express = require('express');
 
 module.exports = function(app, config) {
   app.configure(function () {
+    app.enable('strict routing');
     app.use(express.compress());
     app.use(express.static(config.root + '/public'));
-    app.use('/theater/', express.static(config.theaterApp + '/dist'));
     app.set('port', config.port);
     app.set('views', config.root + '/app/views');
     app.set('view engine', 'jade');
@@ -13,6 +13,8 @@ module.exports = function(app, config) {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
+    //Enable Theater APP statics
+    app.use('/theater/', express.static(config.theaterApp + '/dist'));
     app.use(function(req, res) {
       res.status(404).sendfile(config.root+'/public/404.html');
     });
--- server/config/routes.js
+++ server/config/routes.js
@@ -4,6 +4,9 @@ module.exports = function(app){
   var theater = require('../app/controllers/theater');
 
   /* Theater routes */
+  app.get('/theater', function(req, res){
+    res.redirect('/theater/');
+  })
   app.get('/theater/data/movies.json', theater.movies);
   app.get('/theater/data/:movieId.json', theater.movie);
 
