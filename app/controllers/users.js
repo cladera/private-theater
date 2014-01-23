9f63f77050bf0baa8cbdc7a67d25cc7cd4a5a3a7
cgcladera <cgcladera@gmail.com>
2014-01-23 18:43:19 +0100

    Add user controller

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -29,6 +29,31 @@ exports.get = function(req, res){
   });
 };
 
+exports.add = function(req, res){
+  if(req.body.email === undefined || req.body.password){
+    return res.send(400);
+  }
+  req.body.role = req.body.role || 'USER';
+
+  var user = new User();
+  user.email = req.body.email;
+  //TODO: Parse email
+  User.count({
+    email: req.body.email
+  }, function(err, count){
+    if(count === 0){
+      User.signUp(req.body.email, req.body.password, req.body.role, function(err, user){
+        if(err) {
+          return res.send(500);
+        }
+        res.json(user.serialize());
+      });
+    }else {
+      return res.send(409);
+    }
+  });
+};
+
 exports.me = function(req, res){
   res.json(req.user.serialize());
 }
--- config/routes.js
+++ config/routes.js
@@ -18,7 +18,7 @@ module.exports = function(app, passport){
   app.get('/users/all', Auth.isAuthenticatedAdmin, users.all);
   app.get('/users/me', Auth.isAuthenticatedUser, users.me);
   app.get('/users/:userId', Auth.isAuthenticatedAdmin, users.get);
-  app.get('/user/session.json', Auth.isAuthenticatedUser, user.session);
+  app.post('/users/new', Auth.isAuthenticatedAdmin, users.add);
 
   /* Client app routes */
   app.get('/index.html', function(req,res){
@@ -34,7 +34,7 @@ module.exports = function(app, passport){
     res.redirect('/admin/');
   });*/
   //app.get('/admin/', Auth.isAuthenticatedAdmin);
-  //app.post('/admin/users/new', Auth.isAuthenticatedAdmin, admin.postUser);
+  app.post('/admin/users/new', Auth.isAuthenticatedAdmin, admin.postUser);
   //app.post('/admin/movies/new', Auth.isAuthenticatedAdmin, admin.postMovie);
   app.get('/admin/movies/remove', Auth.isAuthenticatedAdmin, admin.removeMovie);
   //app.get('/admin/users', admin.users);
