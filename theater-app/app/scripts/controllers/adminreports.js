37aa635510198b74f160ed4a5457d227f50c8ba0
cgcladera <cgcladera@gmail.com>
2014-03-12 15:03:59 +0100

    Adds buttons behaviour

--- theater-app/app/scripts/controllers/adminreports.js
+++ theater-app/app/scripts/controllers/adminreports.js
@@ -3,4 +3,9 @@
 angular.module('privateTheaterApp')
   .controller('AdminReportsCtrl', ['$scope', 'MediaError', function ($scope, MediaError) {
     $scope.reports = MediaError.query();
+    $scope.updateStatus = function (report, status){
+      var prevStatus = report.status;
+      report.status = status;
+      //TODO: Update report
+    };
   }]);
