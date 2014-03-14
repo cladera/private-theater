62070da28651828a4136d838b4cfc25421cf85be
cgcladera <cgcladera@gmail.com>
2014-03-14 14:03:36 +0100

    Replaces regular bootstrap alert by angular-bootstrap directive

--- theater-app/app/scripts/app.js
+++ theater-app/app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('privateTheaterApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ui.bootstrap'
 ])
   .config(['$routeProvider', '$locationProvider',function ($routeProvider) {
     var access = routingConfig.accessLevels;
