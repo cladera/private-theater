f52a2f92474d4e1e8803f723f0eba74dd3a5efcf
cgcladera <cgcladera@gmail.com>
2014-03-20 20:14:26 +0100

    Admin reports tests

--- theater-app/test/spec/controllers/adminreports.js
+++ theater-app/test/spec/controllers/adminreports.js
@@ -1,23 +1,99 @@
 'use strict';
 
 describe('Controller: AdminReportsCtrl', function () {
-
+  beforeEach(function(){
+    this.addMatchers({
+      toEqualData: function(expected) {
+        return angular.equals(this.actual, expected);
+      }
+    });
+  });
   // load the controller's module
   beforeEach(module('privateTheaterApp'));
 
-  var AdminreportsCtrl,
-    scope;
+  var AdminReportsCtrl,
+    scope,
+    $httpBackend,
+    xyzReports = function(){
+      return [
+        {
+          _id: 0,
+          body: 'Body 1',
+          status: 'open',
+          movie: 1
+        },
+        {
+          _id: 1,
+          body: 'Body 2',
+          status: 'closed',
+          movie: 1
+        },
+        {
+          _id: 2,
+          body: 'Body 3',
+          status: 'discarted',
+          movie: 2
+        }
+      ];
+    };
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/reports/all')
+      .respond(xyzReports());
     scope = $rootScope.$new();
-    AdminreportsCtrl = $controller('AdminReportsCtrl', {
+    AdminReportsCtrl = $controller('AdminReportsCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    //TODO: AdminReports tests
-    expect(true).toBe(true);
+  it('should fetch reports list', function () {
+    $httpBackend.flush();
+    expect(scope.reports).toEqualData(xyzReports());
   });
+
+  it('should status filter to be open initially', function(){
+    $httpBackend.flush();
+    expect(scope.statusFilter).toBe('open');
+  });
+
+  it('should status classes to be created', function(){
+    $httpBackend.flush();
+    expect(scope.statusClasses).toEqualData( {
+      open: '',
+      closed: 'success',
+      discarted: 'active'
+    });
+  });
+
+  it('should status labels to be created', function(){
+    $httpBackend.flush();
+    expect(scope.statusLabels).toEqualData({
+      open: 'label-info',
+      closed: 'label-success',
+      discarted: 'label-danger'
+    });
+  });
+
+  /* TODO: Test update report
+  it('should save report status', function(){
+    console.log(scope.updateStatus);
+    $httpBackend.flush();
+    var reports = xyzReports(),
+      updatedReport = {
+        _id: reports[0]._id,
+        body: reports[0].body,
+        status: 'closed',
+        movie: reports[0].movie
+
+      };
+    reports[0].$update = function(){
+
+    };
+    $httpBackend.expectPUT('reports/'+reports[0]._id).respond(200, updatedReport);
+    scope.updateStatus(reports[0], 'closed');
+    $httpBackend.flush();
+    expect(reports[0]).toEqualData(updatedReport);
+  });*/
 });
